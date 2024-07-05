ef92ed55497a4a0a02d2709239dcb7d1a71aa0bd
OGD <114223604+OGD311@users.noreply.github.com>
2024-07-05 21:53:46 +0100

    Tweaks

---
+++ func/unitConvert.py
@@ -0,0 +1,23 @@
+{
+    "SI_units_of_volume": {
+        "m^3": 1,
+        "km^3": 1e9,
+        "hm^3": 1e6,
+        "dam^3": 1e3,
+        "m^3": 1,
+        "dm^3": 1e-3,
+        "cm^3": 1e-6,
+        "mm^3": 1e-9
+    },
+
+    "SI_units_of_length": {
+        "m": 1,
+        "km": 1e3,
+        "hm": 1e2,
+        "dam": 1e1,
+        "dm": 1e-1,
+        "cm": 1e-2,
+        "mm": 1e-3
+    }
+
+}
---
+++ test.py
@@ -0,0 +1,9 @@
+from ChemPy import ChemPy
+
+chem= ChemPy('M')
+
+chem.moles_mass_mr(mass=20, moles=2)
+
+chem.number_moles_water_in_hydrated(0.017, 0.029)
+
+chem.pH(1e-4)
